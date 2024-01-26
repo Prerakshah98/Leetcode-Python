c4db70bb713b5070c08299fdea9faddbd3498302
Prerakshah98 <prerak1998@gmail.com>
2024-01-25 18:34:28 -0800

    New file

---
+++ Easy/509_Fibonacci Number.py
@@ -0,0 +1,54 @@
+"""The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+
+F(0) = 0, F(1) = 1
+F(n) = F(n - 1) + F(n - 2), for n > 1.
+Given n, calculate F(n).
+
+ 
+
+Example 1:
+
+Input: n = 2
+Output: 1
+Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+Example 2:
+
+Input: n = 3
+Output: 2
+Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
+Example 3:
+
+Input: n = 4
+Output: 3
+Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
+ 
+
+Constraints:
+
+0 <= n <= 30
+"""
+class Solution:
+    def fib(self, n: int) -> int:
+
+        l = [0,1]
+        if n in l:
+            return n
+        for i in range(2,n+1):
+            l.append(l[i-1]+l[i-2])
+        
+        return l[-1]
+
+# Test Cases
+if __name__ == "__main__":
+    sol = Solution()
+    test_cases = [   #n values
+        0,
+        1,  
+        2, 
+        5,  
+        10
+    ]
+
+    for i, case in enumerate(test_cases, 1):
+        result = sol.fib(case)
+        print(f"Test Case {i}: n = {case}, Fibonacci Number = {result}")
