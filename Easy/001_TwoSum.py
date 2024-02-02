1ee631e12dc966e4393c93b7006bec3e7cbc03ca
Prerakshah98 <prerak1998@gmail.com>
2024-02-01 16:14:15 -0800

    New file

--- Easy/001_TwoSum.py
+++ Easy/001_TwoSum.py
@@ -35,11 +35,18 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(i+1 , len(nums)):
-                sum = nums[i] + nums[j]
-                if sum  == target:
-                    return[i,j]
+        # for i in range(len(nums)):
+        #     for j in range(i+1 , len(nums)):
+        #         sum = nums[i] + nums[j]
+        #         if sum  == target:
+        #             return[i,j]
+        
+        dict={}
+        for i,n in enumerate(nums):
+            if n in dict:
+                return dict[n],i
+            else:
+                dict[target-n]=i
 
 if __name__ == "__main__":
     sol = Solution()
